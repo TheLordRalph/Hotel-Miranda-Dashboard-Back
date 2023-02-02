2059d029e5c1df7ee415c52a96863ee150b82a80
TheLordRalph <raulmoretamartin@gmail.com>
2023-02-02 15:04:24 +0100

    Añadida query para obtener una sola room especifica.

--- dist/controllers/rooms.js
+++ dist/controllers/rooms.js
@@ -5,8 +5,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.deleteRoom = exports.putRoom = exports.postRoom = exports.getRoom = exports.getRooms = void 0;
 const conection_1 = __importDefault(require("../data/conection"));
-const getRooms = async (req, res) => {
-    const rooms = await new Promise((resolve, reject) => {
+const getRooms = async (req, res, next) => {
+    let rooms = await new Promise((resolve, reject) => {
         conection_1.default.query('SELECT * FROM rooms;', (err, rows) => {
             if (err)
                 reject(err);
@@ -14,6 +14,7 @@ const getRooms = async (req, res) => {
         });
     });
     for (let i = 0; i < rooms.length; i++) {
+        // Get de las imagenes de cada room
         const imagesRooms = await new Promise((resolve, reject) => {
             conection_1.default.query('SELECT I.imagen FROM imagesRooms I INNER JOIN images_relacion_rooms IR ON IR.idImagesRoom = I.idImagesRoom WHERE IR.idHabitacion = ?;', rooms[i].idHabitacion, (err, rows) => {
                 if (err)
@@ -21,6 +22,7 @@ const getRooms = async (req, res) => {
                 return resolve(rows);
             });
         });
+        // Get de las facilities de cada room
         const facilitiesRooms = await new Promise((resolve, reject) => {
             conection_1.default.query('SELECT F.facility FROM facilities F INNER JOIN facilities_relacion_rooms FR ON FR.idFacilities = F.idFacilities WHERE FR.idHabitacion = ?;', rooms[i].idHabitacion, (err, rows) => {
                 if (err)
@@ -28,23 +30,68 @@ const getRooms = async (req, res) => {
                 return resolve(rows);
             });
         });
+        // Creo un array de imagenes con los valores de los objetos que obtuve en la SELECT
         let arrayImages = [];
         for (let f = 0; f < imagesRooms.length; f++) {
             arrayImages.push(imagesRooms[f].imagen);
         }
+        // Creo un array de facilities con los valores de los objetos que obtuve en la SELECT
         let arrayFacilities = [];
         for (let f = 0; f < facilitiesRooms.length; f++) {
             arrayFacilities.push(facilitiesRooms[f].facility);
         }
+        // A cada objeto de room le incorporo su array de facilities e imagenes.
         rooms[i].images = arrayImages;
         rooms[i].facilities = arrayFacilities;
     }
     res.status(200).json(rooms);
 };
 exports.getRooms = getRooms;
-const getRoom = (req, res) => {
-    // const { id } = req.query.id;
-    // res.status(200).json(rooms.filter(room => room.idHabitacion === id));
+const getRoom = async (req, res, next) => {
+    try {
+        const param = parseInt(req.params.roomId);
+        const query = `SELECT * FROM rooms WHERE idHabitacion = ${param};`;
+        let room = await new Promise((resolve, reject) => {
+            conection_1.default.query(query, (err, rows) => {
+                if (err)
+                    reject(err);
+                return resolve(rows);
+            });
+        });
+        const imagesRoom = await new Promise((resolve, reject) => {
+            const queryImages = `SELECT I.imagen FROM imagesRooms I INNER JOIN images_relacion_rooms IR ON IR.idImagesRoom = I.idImagesRoom WHERE IR.idHabitacion = ${param};`;
+            conection_1.default.query(queryImages, (err, rows) => {
+                if (err)
+                    reject(err);
+                return resolve(rows);
+            });
+        });
+        const facilitiesRoom = await new Promise((resolve, reject) => {
+            const queryFacilities = `SELECT F.facility FROM facilities F INNER JOIN facilities_relacion_rooms FR ON FR.idFacilities = F.idFacilities WHERE FR.idHabitacion = ${param};`;
+            conection_1.default.query(queryFacilities, (err, rows) => {
+                if (err)
+                    reject(err);
+                return resolve(rows);
+            });
+        });
+        // Creo un array de imagenes con los valores de los objetos que obtuve en la SELECT
+        let arrayImages = [];
+        for (let f = 0; f < imagesRoom.length; f++) {
+            arrayImages.push(imagesRoom[f].imagen);
+        }
+        // Creo un array de facilities con los valores de los objetos que obtuve en la SELECT
+        let arrayFacilities = [];
+        for (let f = 0; f < facilitiesRoom.length; f++) {
+            arrayFacilities.push(facilitiesRoom[f].facility);
+        }
+        // A la room le incorporo su array de facilities e imagenes.
+        room[0].images = arrayImages;
+        room[0].facilities = arrayFacilities;
+        res.status(200).json(room);
+    }
+    catch (error) {
+        next(error);
+    }
 };
 exports.getRoom = getRoom;
 const postRoom = (req, res) => {
--- src/controllers/rooms.ts
+++ src/controllers/rooms.ts
@@ -1,6 +1,5 @@
+import { Request, Response, NextFunction } from 'express';
 import connection from '../data/conection';
-
-
 
 interface roomsI {
   idHabitacion: number,
@@ -8,41 +7,44 @@ interface roomsI {
   roomType: string,
   price: number,
   offerPercent: number,
-  status: string
+  status: string,
+  imagen: string[],
+  facilities: string[]
 }
 
-export const getRooms = async (req:any, res:any) => {
-  const rooms = await new Promise<any>((resolve:any, reject:any) => {
+export const getRooms = async (req:Request, res:Response, next: NextFunction) => {
+  let rooms = await new Promise<any>((resolve:any, reject:any) => {
       connection.query('SELECT * FROM rooms;', (err:any, rows:any) => {
         if (err) reject(err);
         return resolve(rows);
       })
   })
   for (let i = 0; i < rooms.length; i++) {
+    // Get de las imagenes de cada room
     const imagesRooms = await new Promise<any>((resolve:any, reject:any) => {
         connection.query('SELECT I.imagen FROM imagesRooms I INNER JOIN images_relacion_rooms IR ON IR.idImagesRoom = I.idImagesRoom WHERE IR.idHabitacion = ?;', rooms[i].idHabitacion, (err:any, rows:any) => {
           if (err) reject(err);
           return resolve(rows);
         })
     });
-
+    // Get de las facilities de cada room
     const facilitiesRooms = await new Promise<any>((resolve:any, reject:any) => {
         connection.query('SELECT F.facility FROM facilities F INNER JOIN facilities_relacion_rooms FR ON FR.idFacilities = F.idFacilities WHERE FR.idHabitacion = ?;', rooms[i].idHabitacion, (err:any, rows:any) => {
           if (err) reject(err);
           return resolve(rows);
         })
     });
-
+    // Creo un array de imagenes con los valores de los objetos que obtuve en la SELECT
     let arrayImages = [];
     for (let f = 0; f < imagesRooms.length; f++) {
       arrayImages.push(imagesRooms[f].imagen);
     }
-
+    // Creo un array de facilities con los valores de los objetos que obtuve en la SELECT
     let arrayFacilities = [];
     for (let f = 0; f < facilitiesRooms.length; f++) {
       arrayFacilities.push(facilitiesRooms[f].facility);
     }
-
+    // A cada objeto de room le incorporo su array de facilities e imagenes.
     rooms[i].images = arrayImages;
     rooms[i].facilities = arrayFacilities;
 
@@ -50,19 +52,61 @@ export const getRooms = async (req:any, res:any) => {
   res.status(200).json(rooms);
 };
 
-export const getRoom = (req:any, res:any) => {
-  // const { id } = req.query.id;
-  // res.status(200).json(rooms.filter(room => room.idHabitacion === id));
+export const getRoom = async (req:Request, res:Response, next: NextFunction) => {
+  try {
+
+    const param = parseInt(req.params.roomId);
+    const query = `SELECT * FROM rooms WHERE idHabitacion = ${param};`;
+    let room = await new Promise<any>((resolve:any, reject:any) => {
+        connection.query(query, (err:any, rows:any) => {
+          if (err) reject(err);
+          return resolve(rows);
+        })
+    })
+
+    const imagesRoom = await new Promise<any>((resolve:any, reject:any) => {
+        const queryImages = `SELECT I.imagen FROM imagesRooms I INNER JOIN images_relacion_rooms IR ON IR.idImagesRoom = I.idImagesRoom WHERE IR.idHabitacion = ${param};`;
+        connection.query(queryImages, (err:any, rows:any) => {
+          if (err) reject(err);
+          return resolve(rows);
+        })
+    })
+    const facilitiesRoom = await new Promise<any>((resolve:any, reject:any) => {
+        const queryFacilities = `SELECT F.facility FROM facilities F INNER JOIN facilities_relacion_rooms FR ON FR.idFacilities = F.idFacilities WHERE FR.idHabitacion = ${param};`;
+        connection.query(queryFacilities, (err:any, rows:any) => {
+          if (err) reject(err);
+          return resolve(rows);
+        })
+    })
+    // Creo un array de imagenes con los valores de los objetos que obtuve en la SELECT
+    let arrayImages = [];
+    for (let f = 0; f < imagesRoom.length; f++) {
+      arrayImages.push(imagesRoom[f].imagen);
+    }
+    // Creo un array de facilities con los valores de los objetos que obtuve en la SELECT
+    let arrayFacilities = [];
+    for (let f = 0; f < facilitiesRoom.length; f++) {
+      arrayFacilities.push(facilitiesRoom[f].facility);
+    }
+    // A la room le incorporo su array de facilities e imagenes.
+    room[0].images = arrayImages;
+    room[0].facilities = arrayFacilities;
+
+    res.status(200).json(room);
+
+  } catch (error) {
+    next(error);
+  }
 };
 
-export const postRoom = (req:any, res:any) => {
+export const postRoom = (req:Request, res:Response, next: NextFunction) => {
   // res.status(200).json(rooms);
 };
 
-export const putRoom = (req:any, res:any) => {
+export const putRoom = (req:Request, res:Response, next: NextFunction) => {
   // res.status(200).json(rooms);
 };
 
-export const deleteRoom = (req:any, res:any) => {
+export const deleteRoom = (req:Request, res:Response, next: NextFunction) => {
   // res.status(200).json(rooms);
 };
